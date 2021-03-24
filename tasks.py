869b038ff63da6cbd9816730e9c448987da0d150
nialov <nikolasovaskainen@hotmail.com>
2021-03-24 12:16:30 +0200

    startup after copier

---
+++ tasks.py
@@ -0,0 +1,103 @@
+"""
+Invoke tasks.
+
+Most tasks employ nox to create a virtual session for testing.
+"""
+from invoke import task
+from invoke import UnexpectedExit
+
+nox_parallel_sessions = (
+    "tests_pipenv",
+    "tests_pip",
+)
+
+package_name = "tracerepo"
+
+
+@task
+def requirements(c):
+    """
+    Sync requirements from Pipfile to setup.py.
+    """
+    c.run("nox --session requirements")
+
+
+@task
+def format(c):
+    """
+    Format everything.
+    """
+    c.run("nox --session format")
+
+
+@task(pre=[format])
+def lint(c):
+    """
+    Lint everything.
+    """
+    c.run("nox --session lint")
+
+
+@task(pre=[requirements])
+def nox_parallel(c):
+    """
+    Run selected nox test suite sessions in parallel.
+    """
+    # Run asynchronously and collect promises
+    print(f"Running {len(nox_parallel_sessions)} nox test sessions.")
+    promises = [
+        c.run(
+            f"nox --session {nox_test} --no-color",
+            asynchronous=True,
+            timeout=360,
+        )
+        for nox_test in nox_parallel_sessions
+    ]
+
+    # Join all promises
+    results = [promise.join() for promise in promises]
+
+    # Check if Result has non-zero exit code (should've already thrown error.)
+    for result in results:
+        if result.exited != 0:
+            raise UnexpectedExit(result)
+
+    # Report to user of success.
+    print(f"{len(results)} nox sessions ran succesfully.")
+
+
+@task(pre=[requirements])
+def ci_test(c):
+    """
+    Test suite for continous integration testing.
+
+    Installs with pip, tests with pytest and checks coverage with coverage.
+    """
+    c.run("nox --session tests_pip")
+
+
+@task(pre=[nox_parallel])
+def test(_):
+    """
+    Run tests.
+
+    This is an extensive suite. It first tests in current environment and then
+    creates virtual sessions with nox to test installation -> tests.
+    """
+
+
+@task(pre=[requirements])
+def docs(c):
+    """
+    Make documentation to docs using nox.
+    """
+    c.run("nox --session docs")
+
+
+@task(pre=[requirements, test, lint, docs])
+def make(_):
+    """
+    Make all.
+    """
+    print("---------------")
+    print("make successful.")
---
+++ tracerepo/rules.py
@@ -0,0 +1,100 @@
+"""
+trace-repository rules.
+"""
+from enum import Enum, unique
+import pandera as pa
+from typing import Dict, Any, Type
+from functools import lru_cache
+
+
+@unique
+class ColumnNames(Enum):
+
+    """
+    Column names for database.csv.
+    """
+
+    AREA = "area"
+    TRACES = "traces"
+    THEMATIC = "thematic"
+    SCALE = "scale"
+    AREA_SHAPE = "area-shape"
+    EMPTY = "empty"
+    VALIDATED = "validated"
+
+
+@unique
+class BooleanChoices(Enum):
+
+    """
+    Boolean chocies in database.csv.
+    """
+
+    TRUE = "true"
+    FALSE = "false"
+
+
+@unique
+class AreaShapes(Enum):
+
+    """
+    area-shape options in database.csv.
+    """
+
+    CIRCLE = "circle"
+    OTHER = "other"
+
+
+@lru_cache(maxsize=None)
+def name_column_kwargs() -> Dict[str, Any]:
+    """
+    Get kwargs for a string/name column.
+    """
+    return dict(
+        pandas_dtype=pa.String,
+        checks=[
+            pa.Check.str_length(cls=pa.Check, min_value=5, max_value=30),
+        ],
+        allow_duplicates=False,
+    )
+
+
+@lru_cache(maxsize=None)
+def enum_column_kwargs(enum_class: Type[Enum]) -> Dict[str, Any]:
+    """
+    Get kwargs for an enum column.
+    """
+    return dict(
+        pandas_dtype=pa.String,
+        checks=pa.Check.isin([member.value for member in enum_class]),
+    )
+
+
+@lru_cache(maxsize=None)
+def database_schema() -> pa.DataFrameSchema:
+    """
+    Get pandera DataFrame schema for database.csv.
+    """
+    return pa.DataFrameSchema(
+        # Index is the area name
+        index=pa.Index(**name_column_kwargs()),
+        # Columns
+        columns={
+            # traces, thematic and scale columns are strings
+            ColumnNames.TRACES.value: pa.Column(**name_column_kwargs()),
+            ColumnNames.THEMATIC.value: pa.Column(**name_column_kwargs()),
+            ColumnNames.SCALE.value: pa.Column(**name_column_kwargs()),
+            # area-shape must be one of the enum values
+            ColumnNames.AREA_SHAPE.value: pa.Column(
+                **enum_column_kwargs(enum_class=AreaShapes)
+            ),
+            # empty must be one of the boolean enum values
+            ColumnNames.EMPTY.value: pa.Column(
+                **enum_column_kwargs(enum_class=BooleanChoices)
+            ),
+            # validated must be one of the boolean enum values
+            ColumnNames.VALIDATED.value: pa.Column(
+                **enum_column_kwargs(enum_class=BooleanChoices)
+            ),
+        },
+    )
