50d469aa002f5994e3de5d932296af99412aa64f
nialov <nikolasovaskainen@hotmail.com>
2021-06-03 10:02:20 +0300

    higher upper str length limit in csv

--- tracerepo/rules.py
+++ tracerepo/rules.py
@@ -192,16 +192,16 @@ def filename_regex(geom_type: Optional[ColumnNames] = None) -> str:
     E.g.
 
     >>> filename_regex()
-    '^[a-z0-9_]{2,23}$'
+    '^[a-z0-9_]{2,50}$'
 
     >>> filename_regex(ColumnNames.AREA)
-    '^[a-z0-9_]{2,23}_area$'
+    '^[a-z0-9_]{2,50}_area$'
 
     >>> filename_regex(ColumnNames.TRACES)
-    '^[a-z0-9_]{2,23}_traces$'
+    '^[a-z0-9_]{2,50}_traces$'
 
     """
-    base = r"^[a-z0-9_]{2,23}"
+    base = r"^[a-z0-9_]{2,50}"
 
     if geom_type is None:
         return base + r"$"
