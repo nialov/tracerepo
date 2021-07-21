70de6a9745e06eb375d264942f0113c02cd45a3d
nialov <nikolasovaskainen@hotmail.com>
2021-07-21 13:29:25 +0300

    build: finish up copier update

---
+++ tests/_profile.py
@@ -0,0 +1,14 @@
+"""
+Script for profiling ``tracerepo`` performance.
+"""
+
+
+def perf_profile():
+    """
+    Profile ``tracerepo`` performance.
+    """
+    return
+
+
+if __name__ == "__main__":
+    perf_profile()
